62f41a3224ed875ad86d0f158bdea17387d26230
Noons-ledev <noah.briet@icloud.com>
2024-10-21 13:45:30 +0000

    printing numbers 1 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main -Entry of program
+ *Description: Print numbers from 0 to 9
+ *Return: Always return 0(Success)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf ("%d\n", n);
+}
+return (0);
+}
