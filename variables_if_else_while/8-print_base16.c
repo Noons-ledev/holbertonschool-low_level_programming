890968f370cf32321a1524e298d392bfb428815e
Noons-ledev <noah.briet@icloud.com>
2024-10-21 14:23:50 +0000

    print base 16 characters

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main-Entry of program
+ *Description: print all hexa char from 0-9 to a-f
+ *Return: Always return 0(Success)
+ */
+int main(void)
+{
+char HexDigit;
+for ( HexDigit = '0'; HexDigit <= '9'; HexDigit++)
+{
+putchar(HexDigit);
+}
+for ( HexDigit = 'a'; HexDigit <= 'z'; HexDigit++)
+{
+putchar(HexDigit);
+}
+putchar('\n');
+return (0);
+}
