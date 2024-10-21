104f2d59b96df0dda316dcb71acb026e99ffe31b
Noons-ledev <noah.briet@icloud.com>
2024-10-21 08:46:44 +0000

    First touch of C, use of puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
-/* main- entry point
- *
- *puts - prints the sentence I want 
- *
- * return 0 for success */
+/* main- entry point */
+/*puts - prints the sentence I want*/ 
+/* return 0 for success */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
