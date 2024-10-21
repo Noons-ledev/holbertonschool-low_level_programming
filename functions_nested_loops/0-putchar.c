31ab89d052bfa4211ef1d753fc06fef9e1c496e4
Noons-ledev <noah.briet@icloud.com>
2024-10-21 15:13:01 +0000

    print putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main- Entry of program
+ *Description:Prints putchar
+ *Return: Always return 0 (Success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
