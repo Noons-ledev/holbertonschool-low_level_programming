24f7ecad4e2d91cebbd62d8d5090d9f2e812f609
Noons-ledev <noah.briet@icloud.com>
2024-10-21 09:43:47 +0000

    printing sizes

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main- Entry of the program
+*
+*Description: prints the size of various type
+*
+*Return: Always return 0 (Success)
+*/
+int main(void)
+{
+  printf("Size of a char: 1byte(s)\n");
+  printf("Size of an int: 4byte(s)\n");
+  printf("Size of a long int: 4byte(s)\n");
+  printf("Size of a long long int: 8byte(s)\n");
+  printf("Size of a float: 4byte(s)\n");
+  return (0);
+}
