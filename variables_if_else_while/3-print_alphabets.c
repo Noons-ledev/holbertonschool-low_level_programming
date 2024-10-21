ea824bf0ec90d9901d1666722c713682136cf702
Noons-ledev <noah.briet@icloud.com>
2024-10-21 13:31:10 +0000

    two alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,7 @@
  *Description: Print alphabet without printf or puts
  *Return: Always return 0(Success)
  */
-int main (void)
+int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
