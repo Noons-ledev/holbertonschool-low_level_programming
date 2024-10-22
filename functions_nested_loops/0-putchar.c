d0e2ca7c1495cfcaa1f642bc699e4bf1b8f61a51
Noons-ledev <noah.briet@icloud.com>
2024-10-22 08:40:53 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include "main.h"
-#include <stdio.h>
 /**
  * _putchar - writes the character c to stdout
  * Return: On success 1.
@@ -15,14 +14,4 @@ return (write(1, &c, 1));
  *Description : Writes all _putchar characters
  *Return: Always return 0 (Success)
  */
-int main(void)
-{
-int i=0;
-char str[]="_putchar";
-for ( i = 0; i < 8; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
-return (0);
-}  
+
