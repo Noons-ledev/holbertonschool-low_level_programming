be6fd8fa3ec77621fe9db72453e8d4464cdfa22a
Noons-ledev <noah.briet@icloud.com>
2024-10-22 12:10:50 +0000

    islower function

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _islower- Checks for lower or uppercase
+* Description : Checks for lower or uppercase 
+* Return: Always return 1 for lowercase (true)
+*/
+
+int _islower(int b)
+{
+return (b >= 'a' && b <= 'z');
+}
