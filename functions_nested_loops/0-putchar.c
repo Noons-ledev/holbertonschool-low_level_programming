5d9af9a95316ee632bf2f930bed91975acd2300f
Noons-ledev <noah.briet@icloud.com>
2024-10-22 09:14:19 +0000

    compiling source code + main.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
-#include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+*main-entry of program
+*Description : Print all blocks of an array 
+*Return: Always return 0(Success)
+*/
+int main(void)
 {
-return (write(1, &c, 1));
+int i;
+char str[]="_putchar";
+for ( i = 0; i < 8; i++)
+{
+_putchar(str[i]);
 }
-/**
- *main-entry point of program
- *Description : Writes all _putchar characters
- *Return: Always return 0 (Success)
- */
-
+_putchar('\n');
+return (0);
+}
