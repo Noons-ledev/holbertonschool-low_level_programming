ccc538619e3a4c91a3c2d4b69829935dea936eec
Noons-ledev <noah.briet@icloud.com>
2024-10-22 14:07:14 +0000

    Last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_last_digit- Print last digit of a given number
+ *@n: Number to compare
+ *@a: Last digit to return
+ *Description:Use of an IF statement
+ *Return: Return the last digit
+ */
+int print_last_digit(int n);
+{
+int a;
+a = n % 10;
+if (n < 0)
+{
+a = -a;
+}
+_putchar(a + '0');
+return (a);
+}
