240b29a909d60f75512ae5020d59dea974c2831a
Noons-ledev <noah.briet@icloud.com>
2024-10-22 12:37:22 +0000

    isalphanumeric

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,8 +4,12 @@
 * Description : Checks for alphanumeric
 * Return: Always return 1 for lowercase (true)
 */
-
 int _isalpha(int b)
 {
-return ((b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z'));
+  if ((b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z'))
+    {
+      return (1);
+    }
+  else
+    return (0);
 }
