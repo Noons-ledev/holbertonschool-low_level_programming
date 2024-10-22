4893418f4f735ab2cbf250314f7a770f5aa8dd46
Noons-ledev <noah.briet@icloud.com>
2024-10-22 13:33:42 +0000

    absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
  *_abs - Return absolute value of given integer
  *Description: Use of if to check if given int is positive or not
  *@a: Character to check
- *Return : Return (a) for absolute value
+ *Return: Return (a) for absolute value
  */
 int _abs(int a)
 {
