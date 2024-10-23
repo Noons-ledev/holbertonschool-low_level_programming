f57943837d4d172cfab18d9fa41f0ba1ecad0d12
Noons-ledev <noah.briet@icloud.com>
2024-10-23 07:56:40 +0000

    time loop

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,18 +12,13 @@ int h1;
 int h2;
 int m1;
 int m2;
- for (heures = 0; heures <= 23;)
+for (heures = 0; heures <= 23;)
 {
 h1 = heures / 10;
 h2 = heures % 10;
 m1 = minutes / 10;
 m2 = minutes % 10;
-if ( minutes == 60)
-{
-heures++;
-minutes = 00;
-}
-while (minutes < 60 )
+for (minutes = 0; minutes <= 59; minutes++)
 { 
 _putchar(h1 + '0') ;
 _putchar(h2 + '0');
@@ -31,7 +26,11 @@ _putchar(':');
 _putchar(m1 + '0');
 _putchar(m2 + '0');
 _putchar('\n');
-minutes++;
+}
+if (minutes == 60)
+{
+heures++;
+minutes = 0;
 }
 }
 }
