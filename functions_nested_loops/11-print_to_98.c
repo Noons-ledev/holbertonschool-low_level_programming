2d8c147e7597593c396f671f2270f70c1024b011
Noons-ledev <noah.briet@icloud.com>
2024-10-23 13:14:16 +0000

    to98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ *paint_to_98 - print all until 98
+ *@n : Printing starts here
+ *Description: According to given number
+ *Return: Always 0 (success)
+ *
+ */
+void print_to_98(int n)
+{
+_putchar((n / 100) + '0');
+_putchar(((n / 10) /10) + '0');
+_putchar((n % 10) + ' ');
+if ( n > 98)
+{
+while ( n > 98)
+{
+int n1 = n - 1;
+_putchar((n1 / 100) + '0');
+_putchar(((n1 / 10) /10) + '0');
+_putchar((n1 % 10) + ' ');
+n--;
+}
+}
+else if (n < 98)
+{
+while ( n < 98)
+{
+int n2 = n + 1;
+if ( n < 0)
+{
+_putchar('-')
+_putchar((n2 / 100) + '0');
+_putchar((n2 / 10) + '0');
+_putchar((n2 % 10) + ' ');
+}
+else
+{
+_putchar(
+_putchar((n2 / 10) + '0');
+_putchar((n2 % 10) + ' ');
+n++;
+}
+}
+}
+}
+     
+	  
