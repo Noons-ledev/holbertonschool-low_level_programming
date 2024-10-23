c6c82a65fa6d02239725cae2ba71bf16039e1233
Noons-ledev <noah.briet@icloud.com>
2024-10-23 09:31:53 +0000

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,34 +15,21 @@ for (b = 0; b <= 9;b++)
 int produit = a * b;
 int p1 = produit / 10;
 int p2 = produit % 10;
-if (produit == 0)
+if (produit < 10)
 {
 _putchar(produit + '0');
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
 }
-else if (produit < 10 && produit != 0)
-{
-_putchar(' ');
-_putchar(produit + '0');
-_putchar(',');
-_putchar(' ');
-}
-else if (produit > 10 && b < 9)
+else
 {
 _putchar(p1 + '0');
 _putchar(p2 + '0');
+}
+if (b < 9)
+{
 _putchar(',');
 _putchar(' ');
 }
-else if (b == 9)
-{
-_putchar(p1 + '0');
-_putchar(p2 + '0');
 _putchar('\n');
-}
-}
 }
 }
 
