4cd494b4bfdd3cb58c6aa2bbdbf2118dbb9d1792
Noons-ledev <noah.briet@icloud.com>
2024-10-23 12:25:36 +0000

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -8,26 +8,5 @@
 int add(int a, int b)
 {
 int res = a + b;
-int r = res / 100;
-int e = (res / 10) / 10;
-int s = res % 10;
-if (res < 0)
-{
-_putchar('-');
-_putchar((res / 100) + '0');
-_putchar(((res / 10) / 10) + '0');
-_putchar((res % 10) + '0');
-}
-else if (res > 100)
-{
-_putchar(r + '0');
-_putchar(e + '0');
-_putchar(s + '0');
-_putchar('\n');
-}
-else if (res < 100) 
-{
-_putchar((res / 10) + '0');
-_putchar((res % 10) + '0');
-}
+return (res);
 }
