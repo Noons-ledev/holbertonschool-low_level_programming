1bb52e532862405b1c801d63d4dfaf5754777715
Noons-ledev <noah.briet@icloud.com>
2024-10-24 07:48:01 +0000

    fwrite to stderr

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main- Entry of program
  *Description: Print a message as stderr
@@ -8,6 +9,7 @@
 
 int main()
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+  fwrite(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",57);
 return (1);
 }
+
