2979a833359d8fec15a46d31070c374308c004ab
Noons-ledev <noah.briet@icloud.com>
2024-10-24 08:16:41 +0000

    100 comb

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,16 +12,12 @@ for (i = 1; i < 90; i++)
 {
 int a = i / 10;
 int b = i % 10;
-if ( a != b || a < b)
+if ( a != b && a < b)
 {
 putchar(a + '0');
 putchar(b + '0');
-}
-if (i != 89)
-{
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n');
 return (0);
