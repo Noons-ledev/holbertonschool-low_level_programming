17c4ba9f396d5a7af295d47574c3443ca591edac
Noons-ledev <noah.briet@icloud.com>
2024-10-24 07:50:29 +0000

    write

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main()
 {
-  fwrite(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",57);
+write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
 return (1);
 }
 
