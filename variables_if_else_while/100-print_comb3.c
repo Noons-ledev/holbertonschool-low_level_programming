d7df42819b008d250f2b937000656cf90c034f19
Noons-ledev <noah.briet@icloud.com>
2024-10-24 08:10:12 +0000

    100 comb

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-entry of program
+ *Description: Prints all numbers avoiding double digits
+ *Return: Always return 0 (success)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 1; i < 90; i++)
+{
+int a = i / 10;
+int b = i % 10;
+if ( a != b || a < b)
+{
+putchar(a);
+putchar(b);
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n')
+}
