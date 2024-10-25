95378d9b86a063eacddc7ebe2766707854d5a72a
Noons-ledev <noah.briet@icloud.com>
2024-10-25 07:33:31 +0000

    tree digits

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-entry of program
+ *Description: Prints all numbers avoiding double digits
+ *Return: Always return 0 (success)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 1; i < 1000; i++)
+{
+int a = i / 100;
+int b = (i / 10) % 10;
+int c = i % 10;
+if ((a < b && b < c) && (a != b && b != c))
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(c + '0');
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
