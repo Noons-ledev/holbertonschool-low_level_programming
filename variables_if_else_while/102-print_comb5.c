54c4bbb5b9852b7a014bb164a6e447868107a9eb
Noons-ledev <noah.briet@icloud.com>
2024-10-25 21:22:22 +0000

    print

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -13,24 +13,16 @@ for (i = 0; i <= 98; i++)
 {
 for (j = 1; j <= 99; j++)
 {
-if ((i / 10) == 0)
-{
-putchar(0 + '0');
-}
-else
-{
 putchar((i / 10) + '0');
-}
 putchar((i % 10) + '0');
 putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
-}
-if (i != 98 && j != 99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
-return (0);
+}
 }
