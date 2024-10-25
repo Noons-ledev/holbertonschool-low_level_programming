ab36c434c599dd0625b177b48cd20dfe60a9529e
Noons-ledev <noah.briet@icloud.com>
2024-10-25 23:08:01 +0000

    print

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *main- calcule la somme des multiples de 3 ou 5
+ *description: ci haut
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+int limit = 1024;
+int i;
+int sum = 0;
+for (i = 0; i < a; i++)
+{
+if (((i % 3) == 0) || ((i % 5) == 0))
+{
+sum = sum + i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
