a308eb4c1f34da28b32ed5491c0311303926d2d1
Noons-ledev <noah.briet@icloud.com>
2024-10-25 07:53:06 +0000

    print numberz

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-entry of program
+ *Description: Prints all numbers avoiding double digits
+ *Return: Always return 0 (success)
+ */
+int main(void)
+{
+int i = 0;
+int j = 0;
+for (i = 0; i <= 99; i++)
+{
+int a = i / 10;
+int b = i % 10;
+int c = j / 10;
+int d = j % 10;
+for (j = 0; j <= 99; j++)
+{
+if (i < j)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(' ');
+putchar(c + '0');
+putchar(d + '0');
+}
+if (i != 98 && j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
