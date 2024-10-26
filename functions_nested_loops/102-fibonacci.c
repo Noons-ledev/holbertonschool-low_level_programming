f2d174e5dee47ab9465a35f32ea69456e66ce233
Noons-ledev <noah.briet@icloud.com>
2024-10-26 16:11:40 +0000

    prep

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -3,11 +3,24 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *main- entry
+ *description: fib 
+ *Return: always 0
  */
 
 int main(void)
 {
-  
+int a = 1;
+int b = 2;
+int n;
+int sum;
+for (n = 0; n < 50; n++)
+{
+printf("%d, ", a);
+sum = a + b;
+a = b;
+b = sum;
+}
+printf("\n");
+return (0);
+}
