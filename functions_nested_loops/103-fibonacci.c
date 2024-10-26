c9939bc29bc4d71c5d6f6513a65bfab9360681f1
Noons-ledev <noah.briet@icloud.com>
2024-10-26 17:02:40 +0000

    fibo sum

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *main- entry
+ *description: fib
+ *Return: always 0
+ */
+
+int main(void)
+{
+double a = 1;
+double b = 2;
+int n;
+double sum;
+int res = 0;
+for (n = 0; n < 50; n++)
+{
+while (a < 4000000)
+{
+sum = a + b;
+res = res + a;
+a = b;
+b = sum;
+}
+}
+printf("%.0f\n", res);
+return (0);
+}
