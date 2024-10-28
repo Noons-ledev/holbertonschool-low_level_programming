131368f20d718431f7b6ecc15bc63bffd8877809
Noons-ledev <noah.briet@icloud.com>
2024-10-28 15:24:24 +0000

    printing one on two

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *puts2- prints 1 on 2 char of a string
+ *@str: points to the first element of the string
+ *description: does it
+ *Return: nothing
+ */
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str += 2;
+}
+}
