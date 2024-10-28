8d60cb9c77ddd8ea9c41d8003dcc8ed4b581303d
Noons-ledev <noah.briet@icloud.com>
2024-10-28 08:37:30 +0000

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul- multiplie deux entiers
+ *@a @b: Characters to multiply
+ *Description: Does what mul does
+ *Return: Product
+ */
+int mul(int a, int b)
+{
+int produit = a * b;
+return (produit);
+}
