fead03e7d423c3056c74983261a6f73673260084
Noons-ledev <noah.briet@icloud.com>
2024-10-28 10:03:46 +0000

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_square- print a size squared of #
+ *@size: Define size of the square
+ *Description: voir au dessus
+ *Return: nothing (void)
+ *
+ */
+void print_square(int size)
+{
+int a;
+int b;
+if (size == 0 || size < 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
