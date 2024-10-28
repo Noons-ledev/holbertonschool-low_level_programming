1bce159dbbc2a25e3967d7dbd7f334b5bdf21da9
Noons-ledev <noah.briet@icloud.com>
2024-10-28 14:35:18 +0000

    print a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts- prints a string
+ *@str: Points to the value of the string
+ *description: does it
+ *Return: Nothing
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+}
