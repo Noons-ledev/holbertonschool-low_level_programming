28ed0be4b80f281ff98b40644713b972ebe47fa4
Noons-ledev <noah.briet@icloud.com>
2024-10-28 10:33:11 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_triangle- print un triangle
+ *@size: Taille a definir
+ *Description: does it
+ *Return: nothing
+ */
+void print_triangle(int size)
+{
+int a;
+int b;
+if (size == 0 || size < 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a <= size; a++)
+{
+for (b = size; b > 0; b--)
+{
+_putchar(' ');
+}
+_putchar('#');
+_putchar('\n');
+}
+}
+}
