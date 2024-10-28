8ab79fd1a7677bdb4cc276a3c01627502126f916
Noons-ledev <noah.briet@icloud.com>
2024-10-28 22:01:48 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *puts_half- prints half of the string depending on its length
+ *@str: pointer used
+ *Description: voila voila
+ *Return: nothing
+ */
+
+void puts_half(char *str)
+{
+int length = 0;
+char *start = str;
+while (*str != '\0')
+{
+s++;
+length++;
+}
+if (length % 2 != 0)
+{
+char *mid = str + ((length - 1) / 2);
+while (*mid != '\0')
+{
+_putchar(*mid);
+mid++;
+}
+_putchar('\n');
+}
+else
+{
+char *mid1 = str + (length / 2);
+while (*start != *mid1)
+{
+_putchar(*start);
+start++;
+}
+_putchar('\n');
+}
+}
