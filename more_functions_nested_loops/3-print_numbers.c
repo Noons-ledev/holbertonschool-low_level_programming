3e6092f93acd6bd411449cd6a70aaf496535bc6d
Noons-ledev <noah.briet@icloud.com>
2024-10-28 08:42:36 +0000

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers- 0 to 9
+ *description: do it 
+ *return: nothing (void)
+ *
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
