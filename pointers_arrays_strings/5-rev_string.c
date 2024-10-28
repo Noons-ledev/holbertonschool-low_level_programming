03ce066394f180152a896e8e678fb25f2070eae6
Noons-ledev <noah.briet@icloud.com>
2024-10-28 15:07:16 +0000

    reversing a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *rev_string- reverse a string
+ *@s: points to the string
+ *description: does it
+ *Return: nothing 
+ *
+ */
+void rev_string(char *s)
+{
+char *start = s;
+char temp;
+while (*s)
+{
+s++;
+}
+s--;
+while (*start < '\0')
+{
+temp = *start;
+*start = *s;
+*s = temp;
+s--;
+start++;
+}
+_putchar('\n'); 
+}
