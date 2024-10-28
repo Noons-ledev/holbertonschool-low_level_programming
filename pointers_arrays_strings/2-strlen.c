606ccea4950ce61c3e63eab072b98841655f0888
Noons-ledev <noah.briet@icloud.com>
2024-10-28 14:25:56 +0000

    length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen- compte la longueur 
+ *description: does it
+ *@s: String to occur
+ *Return: Value of length
+ *
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+s++;
+length ++;
+}
+return (length);
+}
