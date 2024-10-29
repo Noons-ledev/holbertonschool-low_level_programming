d63e023e2469d89422769f4f9e7b52604ddbc312
Noons-ledev <noah.briet@icloud.com>
2024-10-29 08:35:46 +0000

    cpy string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcpy- Cpy a string
+ *@dest: buffer to fill
+ *@src: buffer to copy
+ *description: voila quoi
+ *return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *ogdest = dest;
+while (*src != 0)
+{
+*dest = *src;
+src++;
+dest++;
+}
+*dest = '\0';
+return (ogdest);
+}
