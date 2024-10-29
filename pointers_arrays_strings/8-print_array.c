fcabf39af943b13d10394a09b93b7f799c4ecc45
Noons-ledev <noah.briet@icloud.com>
2024-10-29 08:13:01 +0000

    array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_array-Prints array until element n 
+ *@a: pointer
+ *@n: element number to print until
+ *description: voir la haut
+ *Return: nothing
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+printf("%d", *(a + i));
+if (i != n)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
