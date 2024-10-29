9b558111074081978ff4548933f709f72d90c871
Noons-ledev <noah.briet@icloud.com>
2024-10-29 10:02:12 +0000

    string into int

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *_atoi(char *s)-Converti a string en integer
+ *@s: Pointeur vers l'element premier du string
+ *description: voila
+ *return: nothing
+ *
+ */
+int _atoi(char *s)
+{
+int sign = 0
+int result = 0;
+bool _numberin = false;
+while (*s != '\0')
+{
+if (*s >= '0' && *s <= '9')
+{
+_numberin = true;
+result = result * 10 + (*s - '0');
+}
+if (*s == '-')
+{
+sign = sign + 1;
+}
+else if (*s == '+')
+{
+sign = sign +1;
+}
+s++; 
+}
+if (sign < 0)
+{
+result = - result;
+}
+if (!_numberin)
+{
+return (0);
+}
+}
