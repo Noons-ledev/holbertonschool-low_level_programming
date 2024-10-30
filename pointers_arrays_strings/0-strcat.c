226b3cc35fab644388404381b7a6a3d9a4b59fc1
Noons-ledev <noah.briet@icloud.com>
2024-10-30 08:24:42 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,38 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *
- *
- *
- *
- *
+ *_strcat- concatenate two strings
+ *@dest: String to modify
+ *@src: string to copy
+ *description: fait ce qui est ecrit
+ *Return: pointeur vers le string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-  int i = 0;
-  int j = 0;
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  while (src[j] != '\0')
-    {
-      j++;
-    }
-  dest = malloc((i + j) * sizeof(char) + 1);
-  char *origin_dest = dest;
-  while (*dest != 0)
-    {
-      
+int i = 0;
+int j = 0;
+int a;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+j++;
+}
+char *origin_dest = dest;
+dest = malloc((i + j) *sizeof(char) + 1);
+for (a = 0; a < i; a++)
+{
+dest[a] = origin_dest[a];
+}
+while (*src != '\0')
+{
+dest[a] = *src;
+a++;
+src++;
+}
+dest[a] = '\0';
+return (*(dest));
+}
