1ef8aaa6fb9cfaf0fdd2e998e74b4157eac80a7d
Noons-ledev <noah.briet@icloud.com>
2024-10-30 13:37:03 +0000

    add file

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,28 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
- *
+ *_strncpy- cpy a string like strncpy
+ *@dest:Where to copy
+ *@src: String to copy
+ *@n: number of characters to copy
+ *description: does it 
+ *Return: nothing
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int a;
-  int src_length = 0;
-  while ( src[src_length] != '\0')
-    {
-      src_length++;
-    }
-  for (a = 0; a < n && src[a] != '\0')
-    {
-      *dest = src[a];
-      dest ++;
-    }
-  if (src_length < n)
-    {
-      *dest ='\0';
-    }
-  else
-    {
-      break;
-    }
-    
+int a;
+int src_length = 0;
+while ( src[src_length] != '\0')
+{
+src_length++;
+}
+for (a = 0; a < n && src[a] != '\0';a++)
+{
+*dest = src[a];
+dest ++;
+}
+if (src_length < n)
+{
+*dest ='\0';
+}
+}
   
