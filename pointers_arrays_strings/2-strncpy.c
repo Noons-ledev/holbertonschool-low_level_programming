6a3797b50accda87a585780af034448f49f7fb07
Noons-ledev <noah.briet@icloud.com>
2024-10-30 13:27:03 +0000

    add file

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,3 +9,24 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+  int a;
+  int src_length = 0;
+  while ( src[src_length] != '\0')
+    {
+      src_length++;
+    }
+  for (a = 0; a < n && src[a] != '\0')
+    {
+      *dest = src[a];
+      dest ++;
+    }
+  if (src_length < n)
+    {
+      *dest ='\0';
+    }
+  else
+    {
+      break;
+    }
+    
+  
