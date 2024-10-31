86fc98f62eb2d818437f7f025cf8482dbd7b1c29
Noons-ledev <noah.briet@icloud.com>
2024-10-31 14:04:25 +0000

    capitalize

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <string.h>
+#include <stdbool.h>
+/**
+ *cap_string-capitalize each word of a string
+ *@s: string to work on
+ *description: does it
+ *Return: s
+ */
+char *cap_string(char *s)
+{
+int j;
+bool foundchar = false;
+const char *special_chars = ",;.!?\"(){} \t\n";
+int i = 0;
+if (s[0] >= 'a' && s[0] <= 'z')
+{
+s[0] = s[0] - 32;
+}
+while (s[i] != '\0')
+{
+for (j = 0; special_chars[j] != '\0'; j++)
+{
+if (s[i] == special_chars[j])
+{
+foundchar = true;
+break;
+}
+}
+if (foundchar)
+{
+if (s[i + 1] != '\0' && s[i+1] >= 'a' && s[i+1] <= 'z')
+{
+s[i+1] = s[i+1] - 32;
+}
+}
+i++;
+}
+return (s);
+}
