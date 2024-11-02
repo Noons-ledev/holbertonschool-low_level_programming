34c5c2eb51dc17ac15403dd240fbae3d9b6476a9
Noons-ledev <noah.briet@icloud.com>
2024-11-02 22:11:41 +0000

    rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *rot13- convert into rot13
+ *@t: string to convert
+ *description:
+ *Return: t
+ */
+char *rot13(char *t)
+{
+char *realone = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char *classique = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+int a, b;
+for (a = 0; t[a] != '\0'; a++)
+{
+for (b = 0; classique[b] != '\0'; b++)
+{
+if (classique[b] == t[a])
+{
+t[a] = realone[b];
+}
+}
+}
+return (t);
+}
+
