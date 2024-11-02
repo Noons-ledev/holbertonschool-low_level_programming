60e26eb30393119687b3b6822a24d89bc4eb3abf
Noons-ledev <noah.briet@icloud.com>
2024-11-02 10:00:33 +0000

    print integer

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,8 +7,11 @@
  */
 void print_number(int n)
 {
-if (n >= 0)
+if (n < 0)
 {
+_putchar('-');
+n = -n;
+}
 if (n < 10)
 _putchar(n + '0');
 else if (n >= 10 && n < 100)
@@ -28,29 +31,5 @@ _putchar((n/1000) + '0');
 _putchar(((n / 100) % 10) + '0');
 _putchar(((n / 10) % 10) + '0');
 _putchar((n % 10) + '0');
-}
-}
-else if (n < 0)
-{
-n = -n;
-if (n < 10)
-{
-_putchar('-');
-_putchar(n + '0');
-}
-else if (n >= 10 && n < 100)
-{
-_putchar('-');
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-}
-else if (n > 100 && n < 1000)
-{
-_putchar('-');
-_putchar((n/1000) + '0');
-_putchar(((n / 100) % 10) + '0');
-_putchar(((n / 10) % 10) + '0');
-_putchar((n % 10) + '0');
-}
 }
 }
