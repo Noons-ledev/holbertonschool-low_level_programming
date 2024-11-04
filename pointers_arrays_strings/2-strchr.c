0a405fabb9d693dc5cf18b7a5c5149e358e5d7ed
Noons-ledev <noah.briet@icloud.com>
2024-11-04 09:31:24 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*_strchr- search for c char in s string
+*@s: Area to process into
+*@c: Search for that character
+*Return: s or NULL
+*/
+char *_strchr(char *s, char c)
+{
+if (s == NULL)
+return (s);
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
+return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,5 @@ char *leet(char *);
 char *rot13(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif 
