27e56ddec1c5cddcd24ad1d32ec05fb593fc5f16
Noons-ledev <noah.briet@icloud.com>
2024-11-04 08:41:28 +0000

    first try

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*_memset- Fills n bytes of memory area
+*@s: Area to process
+*@b: Fill with that constant char
+*@n: Fill all n bytes
+*Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int a;
+if (n == 0)
+return (s);
+for (a = 0; a < n; a++)
+{
+s[a] = b;
+}
+return (s);
+}
