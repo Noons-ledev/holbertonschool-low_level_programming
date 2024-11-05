8e2d24f8cd2b26d7088ba4ad2155ad2bce21794f
Noons-ledev <noah.briet@icloud.com>
2024-11-05 10:25:31 +0000

    t2

--- recursion/2-main.c
+++ recursion/2-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     int n;
 
-    n = _strlen_recursion("Corbin Coleman");
+    n = _strlen_recursion("Corbin Coleman ou alors son pote");
     printf("%d\n", n);
     return (0);
 }
