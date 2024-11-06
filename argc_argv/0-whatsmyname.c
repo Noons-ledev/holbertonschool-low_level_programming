a58b55d0b4e8255282aa4b6e1890608dbf96f6b2
Noons-ledev <noah.briet@icloud.com>
2024-11-06 08:42:48 +0000

    t1

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-Prints program name
+ *@argc: count
+ *@argv: array
+ *Return: Always zero (main)
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;  
+printf("%s\n", argv[0]);
+return (0);
+}
