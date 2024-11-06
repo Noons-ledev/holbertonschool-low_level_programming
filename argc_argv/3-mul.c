c093e5a706b9997c4219b3a4baaea80af1988426
Noons-ledev <noah.briet@icloud.com>
2024-11-06 10:10:50 +0000

    t2

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,14 +9,14 @@
 int main(int argc, char *argv[])
 {
 int produit, num1, num2;
-if (argc < 3)
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-produit = num1 * num2;
+produit = num1 *num2;
 printf("%d\n", produit);
 return (0);
 }
