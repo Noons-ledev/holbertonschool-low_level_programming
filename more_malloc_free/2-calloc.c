cb6075a80e8a0b4ef0edc3a49126321ed6cb3583
Noons-ledev <noah.briet@icloud.com>
2024-11-12 21:57:17 +0000

    calloc easy

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,6 +10,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 int a;
 unsigned int product = nmemb * size;
+if (size == 0 || nmemb == 0)
+return (NULL);
 void *array = (char *)malloc(product);
 if (array == NULL)
 return (NULL);
