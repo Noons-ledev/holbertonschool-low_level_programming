cff2288cea287bb6639c521b4acf525726f0354d
Noons-ledev <noah.briet@icloud.com>
2024-11-12 12:55:50 +0000

    t4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *free_grid- frees a 2d grid
+ *@grid: Grid to work on
+ *@height: Value needed to loop 
+ *Return: nothing void
+ */
+
+void free_grid(int **grid, int height)
+{
+int a;
+for (a = 0; a < height; a++)
+{
+free(grid[a]);
+}
+free(grid);
+}
