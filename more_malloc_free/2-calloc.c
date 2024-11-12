c733244d04f3d8ed5033b00bc28dff8e55ff2532
Noons-ledev <noah.briet@icloud.com>
2024-11-12 21:56:04 +0000

    calloc easy

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_calloc- Fais calloc
+ *@nmemb: number of elements
+ *@size: Size of elements
+ *Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int a;
+unsigned int product = nmemb * size;
+void *array = (char *)malloc(product);
+if (array == NULL)
+return (NULL);
+for (a = 0; a < product)
+*(array + a) = 0;
+return (array);
+}
