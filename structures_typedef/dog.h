5027015b6ac15cb624e7553438526038d9c71063
Noons-ledev <noah.briet@icloud.com>
2024-11-13 12:10:26 +0000

    t4

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,5 +16,5 @@ char *owner;
 dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
