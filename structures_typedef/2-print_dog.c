d6e5afe90e729bd12ebd15d1baa89e538585b295
Noons-ledev <noah.briet@icloud.com>
2024-11-13 10:04:36 +0000

    t2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,9 +12,11 @@ if (d == NULL)
 return;
 if (d->name == NULL)
 printf("Name: (nil)\n");
+else
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
+else
 printf("Owner: %s\n", d->owner);
 }
