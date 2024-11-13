0a693a8b0ffa6e81c272dd9aa5cc7eb90e60a389
Noons-ledev <noah.briet@icloud.com>
2024-11-13 09:17:21 +0000

    t0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-/**
- *struct dog- our structure containing datas
- *@name: nom
- *@age: age
- *@owner: proprietaire
- */
+
 struct dog
 {
 char *name;
 float age;
 ochar *owner;
+/*struct dog- structure for our dog*/
 };
 
 
