455c8cae98682b4439cf9385c6c564a0b3d23676
Noons-ledev <noah.briet@icloud.com>
2024-11-13 10:11:41 +0000

    t3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,13 @@
  *@owner:Owner of the doggo
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}
+dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
