0ffefbdce5b9313606447bfb7833e5f0fe49e896
Noons-ledev <noah.briet@icloud.com>
2024-11-19 07:49:58 +0100

    t3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "calc.h"
+
+/**
+*main-entry of program
+*@argc: arg count
+*@argv: Arrays of pointers to argcs
+*Return: always 0 (success)
+*/
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+	printf("Error\n");
+	exit(98);
+	}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+operator = get_op_func(argv[2]);
+
+printf("%d\n", operator(num1, num2));
+return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "calc.h"
+/**
+*op_add- addition de deux integers
+*@a: First one
+*@b: Second one
+*Return: Sum value
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*op_sub-returns substracting b from a
+*@a: Integer1
+*@b: Integer2
+*Return: Value of difference
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*op_mul-Product of a and b
+*@a: Integer1
+*@b: Integer2
+*Return: Product
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+*op_div-Division of a and b
+*@a: Integer1
+*@b: Integer2
+*Return: result
+*/
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	return (a / b);
+}
+
+/**
+*op_mod-Modulo of a div by b
+*@a: Integer1
+*@b: Integer2
+*Return: Modulo
+*/
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+	return (a % b);
+}
