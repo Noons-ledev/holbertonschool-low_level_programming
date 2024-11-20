4696dd043812264cfde56259f4cd42d3482600e6
Noons-ledev <noah.briet@icloud.com>
2024-11-20 09:55:44 +0100

    fix t1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,23 +3,24 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
-*print_numbers- Print all numbers passed as args 
+*print_numbers- Print all numbers passed as args
 *@separator: to print between every number
 *@n: number of arg passed to the function
 *Return: nothing void
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int a;
-    va_list abc;
-    va_start(abc, n);
-    for (a = 0; a < n; a++)
-    {
-        if (separator != NULL)
-        printf("%d%s", va_arg(abc, int), separator);
-    else 
-    printf("%d", va_arg(abc, int));
-    }
-    va_end(abc);
-    printf("\n");
+unsigned int a = 0;
+va_list abc;
+va_start(abc, n);
+while (a != (n - 1))
+{
+if (separator != NULL)
+printf("%d%s", va_arg(abc, int), separator);
+else
+printf("%d", va_arg(abc, int));
+a++;
+}
+printf("%d\n", va_arg(abc, int));
+va_end(abc);
 }
