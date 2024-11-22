d2d4723ab9589d46d9072fb02ec21e3450b76435
Noons-ledev <noah.briet@icloud.com>
2024-11-22 14:53:50 +0100

    t3?

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+*print_c-prints a char
+*@arg: list I'm on
+*Return: nothing void
+*/
+void print_c(va_list arg)
+{
+char i = va_arg(arg, int);
+printf("%c", i);
+}
+/**
+*print_i- Prints an integer
+*@arg: List I'm working on
+*Return: nothing void
+*/
+void print_i(va_list arg)
+{
+int i = va_arg(arg, int);
+printf("%d", i);
+}
+/**
+*print_f-prints a float (converted into a double)
+*@arg: List
+*Return: nothing void
+*/
+void print_f(va_list arg)
+{
+float a = va_arg(arg, double);
+printf("%f", a);
+}
+/**
+*print_str-prints a string
+*@arg:list
+*Return: Nothing void
+*/
+void print_str(va_list arg)
+{
+char *str = va_arg(arg, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
+}
+/**
+*print_all- prints all arguments matching using specifiers
+*@format: Constant pointer to a constant string
+*Return: Nothing void
+*/
+void print_all(const char * const format, ...)
+{
+int i = 0;
+int j;
+int espace = 0;
+va_list arg;
+char *to_find = "cifs";
+void (*fctn_call[4])(va_list) = {print_c, print_i, print_f, print_str};
+va_start(arg, format);
+while (format && format[i])
+{
+j = 0;
+while (to_find[j] != '\0')
+{
+if (format[i] == to_find[j])
+{
+if (espace)
+{
+printf(", ");
+}
+fctn_call[j](arg);
+espace = 1;
+break;
+}
+j++;
+}
+i++;
+}
+va_end(arg);
+printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_c(va_list arg);
+void print_i(va_list arg);
+void print_f(va_list arg);
+void print_str(va_list arg);
 
 
 #endif
