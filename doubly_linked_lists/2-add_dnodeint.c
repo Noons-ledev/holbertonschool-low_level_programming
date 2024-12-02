7953e9a0530d0cfb422c77d5f88cc4cffbf1a991
Noons-ledev <noah.briet@icloud.com>
2024-12-02 13:32:51 +0100

    t2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*add_dnodeint- add a node at the beginning of the list
+*@head: Pointer to the first node
+*@n: int element in the struct
+*Return: pointer to the new element
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	return (NULL);
+new->prev = NULL;
+new->next = *head;
+new->n = n;
+if (*head != NULL)
+{
+(*head)->prev = new;
+}
+*head = new;
+return (new);
+}
