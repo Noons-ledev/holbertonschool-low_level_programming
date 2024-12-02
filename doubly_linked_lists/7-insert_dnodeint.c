d52227cc0765953100531c3132d12fbec8c3d309
Noons-ledev <noah.briet@icloud.com>
2024-12-02 20:14:09 +0100

    fix t7 checking if node+1 exists

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 		temp = temp->next;
 	}
+	if (temp->next == NULL)
+	return (NULL);
+else
+{
 new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	return (NULL);
@@ -33,4 +37,5 @@ temp->next->prev = new;
 new->next = temp->next;
 temp->next = new;
 return (new);
+}
 }
