7cc56e52f9abff87567f8941c4b29dee79170e56
Noons-ledev <noah.briet@icloud.com>
2024-12-02 13:14:44 +0100

    T1 len of list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*dlistint_len- Return nb of nodes in the list
+*@h: Pointer to the first node
+*Return: number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
