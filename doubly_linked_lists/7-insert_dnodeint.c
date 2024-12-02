0120d26cd93586d41c99bb9335a4b646cca959c6
Noons-ledev <noah.briet@icloud.com>
2024-12-02 21:48:12 +0100

    t7 edge case if list does not exist or index = 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,26 +16,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 	unsigned int i = 0;
 	dlistint_t *new;
-
-	for (; i < idx; i++)
+	/**Si la liste n'existe pas*/
+	if (temp == NULL)
+	return (NULL);
+/**Si on demande de créer la liste via idx 0 */
+	if (idx == 0)
+	{
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	*h = new;
+	return (new);
+	}
+	/**Si la liste et les index existent déjà*/
+	for (; i < idx ; i++)
 	{
 		if (temp == NULL)
 		return (NULL);
 		temp = temp->next;
 	}
-	if (temp == NULL)
-	return (NULL);
-else
-{
 new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	return (NULL);
 new->n = n;
+if (temp != NULL)
+{
 new->prev = temp;
 	if (temp->next != NULL)
 	temp->next->prev = new;
 new->next = temp->next;
 temp->next = new;
-return (new);
 }
+return (new);
 }
