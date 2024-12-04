b87f5907eb09380456f8262f2e0d3c9ae94e2c69
Noons-ledev <noah.briet@icloud.com>
2024-12-04 15:45:43 +0100

    t2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ return (i);
 /**
 *append_text_to_file- Lis lettersx letters of a file
 *@filename: Pointer to the file
-*@letters: Numbers of letters to read
+*@text_content: String to append
 *Return: 1 on success, -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
@@ -29,12 +29,12 @@ ssize_t written;
 
 	if (filename == NULL)
 	return (-1);
-fd = open(filename,O_RDWR | O_APPEND);
+fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 	return (-1);
 written = write(fd, text_content, _strlen(text_content));
 if (written == -1)
-	{	
+	{
 	close(fd);
 	return (-1);
 	}
