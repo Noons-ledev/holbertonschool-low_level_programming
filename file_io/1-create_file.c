5668a47d1a5d94f3b5c0f8f1175c9800116468f0
Noons-ledev <noah.briet@icloud.com>
2024-12-04 14:39:57 +0100

    t1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -28,7 +28,7 @@ int fd;
 ssize_t written;
 if (filename == NULL)
 	return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 	return (-1);
 if (text_content == NULL)
