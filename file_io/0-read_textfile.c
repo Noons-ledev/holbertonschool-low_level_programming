bcd48a19a0a2424ef34597b24830bd7bfb295b15
Noons-ledev <noah.briet@icloud.com>
2024-12-04 10:06:33 +0100

    t0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+*read_textfile- Lis lettersx letters of a file
+*@filename: Pointer to the file
+*@letters: Numbers of letters to read
+*Return: Count of letters it could print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buffer;
+ssize_t written, readbytes;
+int fd;
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+	return (0);
+buffer = malloc(letters * sizeof(char));
+if (buffer == NULL)
+	{
+	close(fd);
+	return (0);
+	}
+readbytes = read(fd, buffer, letters);
+if (readbytes == -1)
+	{
+	free(buffer);
+	close(fd);
+	return (0);
+	}
+written = write(STDOUT_FILENO, buffer, letters);
+if (written == -1 || written != readbytes)
+{
+	free(buffer);
+	close(fd);
+	return (0);
+}
+free(buffer);
+close(fd);
+return (written);
+}
+
