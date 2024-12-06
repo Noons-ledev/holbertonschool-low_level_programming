372f453fc9ab3b163ad831168d0bf1906dc1deb2
Noons-ledev <noah.briet@icloud.com>
2024-12-06 17:35:25 +0100

    fix /2

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -25,7 +25,7 @@ return (i);
 */
 unsigned int binary_to_uint(const char *b)
 {
-	int b_len, a, result = 0;
+	int a, result = 0;
 	if (b == NULL)
 	return (0);
 for (a = 0; b[a] != '\0'; a++)
@@ -37,5 +37,5 @@ for (a = 0; b[a] != '\0'; a++)
 	if (b[a] != '0' && b[a] != '1')
 	return (0);
 }
-return (result);
+return (result / 2);
 }
