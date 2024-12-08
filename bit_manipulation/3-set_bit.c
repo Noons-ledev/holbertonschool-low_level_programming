6c74231c1fa985ce1a55c5b8c026476539a27912
Noons-ledev <noah.briet@icloud.com>
2024-12-08 22:59:48 +0100

    kernel style add on t3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,6 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*set_bit- sets the value of the nth bit of a number to 1
+*@n:pointer to number to check on
+*@index: nth bit to check in the number
+*Return: the bit
+*/
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int n_bits;
+
 	n_bits = sizeof(*n) * 8;
 	if (index >= n_bits)
 	return (-1);
