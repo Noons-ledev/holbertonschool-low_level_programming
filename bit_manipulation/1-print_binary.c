168e7e0b9e4edfaf890d816ba92fec85b4bbdadc
Noons-ledev <noah.briet@icloud.com>
2024-12-08 19:37:15 +0100

    t1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*print_binary- Prints the binary representation of a number
+*@n: Number passed as an argument 
+*Return: nothing void
+*/
+void print_binary(unsigned long int n)
+{
+	int n_bits = sizeof(n) * 8;
+	int i;
+
+	for (i = bits - 1; i = 0; i--)
+	{
+	if ((num >> i) & 1)
+	printf("1");
+	else
+		printf("0");
+	}
+	printf("\n");
+}
