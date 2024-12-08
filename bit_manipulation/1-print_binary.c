841dff62f40ac06d4cf9cc0a3f8fc4da70fadff1
Noons-ledev <noah.briet@icloud.com>
2024-12-08 19:42:11 +0100

    fix t1 adding case where n is zero

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -3,7 +3,7 @@
 #include "main.h"
 /**
 *print_binary- Prints the binary representation of a number
-*@n: Number passed as an argument 
+*@n: Number passed as an argument
 *Return: nothing void
 */
 void print_binary(unsigned long int n)
@@ -11,6 +11,11 @@ void print_binary(unsigned long int n)
 	int n_bits = sizeof(n) * 8;
 	int i;
 
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
 	for (i = n_bits - 1; i >= 0; i--)
 	{
 	if ((n >> i) & 1)
