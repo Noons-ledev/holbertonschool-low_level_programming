e2344ecff20e7d019662cb7ef867c042227946eb
Noons-ledev <noah.briet@icloud.com>
2024-12-08 22:57:32 +0100

    forgot to add header

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,4 +4,5 @@ int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *b);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
