07dcc79ee4f4112bbd4db2709f67d315f5205f6a
Noons-ledev <noah.briet@icloud.com>
2024-12-08 19:38:01 +0100

    t1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -11,12 +11,12 @@ void print_binary(unsigned long int n)
 	int n_bits = sizeof(n) * 8;
 	int i;
 
-	for (i = bits - 1; i = 0; i--)
+	for (i = n_bits - 1; i = 0; i--)
 	{
 	if ((num >> i) & 1)
 	printf("1");
 	else
-		printf("0");
+	printf("0");
 	}
 	printf("\n");
 }
